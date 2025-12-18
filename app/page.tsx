b5210f691d1e1cd06525c928623885ab127d0ad5
darkblade <darkblade1078@gmail.com>
2025-12-17 22:57:51 -0600

    Data

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to My Next.js App</h1>
+      <h1>Welcome to the home page</h1>
     </div>
   );
 }
